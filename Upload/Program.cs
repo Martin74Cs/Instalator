ce32b8bbe2910447eb12fc867315f7f577067ebd
Martin Csato <mcsato@seznam.cz>
2023-11-24 17:09:18 +0100

    Přidat upload

---
+++ Upload/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Library;
+
+Console.WriteLine("Poslat soubor na WEB .....");
+string cesta = @"D:\OneDrive\Databaze\Tezak\XMLTablulka1\Setup\Debug\Instal.msi";
+string SoubourCode = await Install.Upload(cesta);
+if (string.IsNullOrEmpty(SoubourCode))
+    Console.WriteLine("Chyba nahrání souboru");
+else
+    Console.WriteLine($"Byl nahran soubor : {SoubourCode}");
